9a1d9317f7586e5083d9f7b7fd623b1288312187
owilling <owilling@ftstudent.org>
2025-05-14 20:05:59 -0400

    commit

--- project.godot/client/Client.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-const ADDRESS = 'localhost'
-const PORT = 9999
-
-var peer = ENetMultiplayerPeer.new()
-
-func _ready():
-	peer.create_client(ADDRESS, PORT)
-	multiplayer.multiplayer_peer = peer
--- project.godot/scripts/MainMenu.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-
-func _on_client_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://client/Client.tscn")
-
-
-func _on_server_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://server/Server.tscn")
